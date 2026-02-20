23e7630f4644d7db3f1926e2e7344ecdaf104993
Subha790 <167208686+Subha790@users.noreply.github.com>
2026-02-20 10:09:54 +0530

    Time: 3 ms (17.47%) | Memory: 19.6 MB (88.22%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.c
@@ -0,0 +1,17 @@
+void moveZeroes(int* nums, int numsSize)
+{
+    int nonzero=0;
+    for(int i=0;i<numsSize;i++)
+    {
+        if(nums[i]!=0)
+        {
+            nums[nonzero++]=nums[i];
+        }
+    }
+    for(int i=nonzero;i<numsSize;i++)
+    nums[i]=0;
+}
+
+
+
+ 
