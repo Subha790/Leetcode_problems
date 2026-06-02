bcd11acea6f03497508e26a85ec9e86ba46f2729
Subha790 <167208686+Subha790@users.noreply.github.com>
2026-06-02 10:04:13 +0530

    Time: 2 ms (44.02%) | Memory: 11 MB (69.03%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.c
@@ -0,0 +1,30 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+char** fizzBuzz(int n, int* returnSize) {
+    char** ans=(char**)malloc(n*sizeof(char*));
+    *returnSize=n;
+    for(int i=1;i<=n;i++)
+    {
+        ans[i-1] = (char*)malloc(10 * sizeof(char));
+        if(i%3==0 && i%5 == 0)
+        {
+            strcpy(ans[i-1],"FizzBuzz");
+        }
+        else if(i%3==0)
+        {
+            strcpy(ans[i-1],"Fizz");
+        }
+        else if(i%5==0)
+        {
+             strcpy(ans[i-1],"Buzz");
+        }
+        else
+        {
+            sprintf(ans[i-1],"%d",i);
+        }
+            
+    }
+    return ans;
+}
+    
